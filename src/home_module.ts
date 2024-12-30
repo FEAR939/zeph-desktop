ed08fb51003a72fc3d20a624bdc5025e7a22de70
FEAR939 <rogronin8877@gmail.com>
2024-12-30 03:09:43 +0100

    redone card hover effect

--- src/home_module.ts
+++ src/home_module.ts
@@ -176,7 +176,7 @@ function home_constructor() {
           ? setSliderIndex(getSliderIndex() - 1)
           : setSliderIndex(getSliderIndex() + 1);
 
-        container.style.transform = `translateX(calc(${getSliderIndex()} * -100%))`;
+        container.style.transform = `translate3D(calc(${getSliderIndex()} * -100%), 0, 0)`;
       };
 
       const categorie_node = document.createElement("div");
@@ -192,7 +192,7 @@ function home_constructor() {
 
       const carousel_previous = document.createElement("button");
       carousel_previous.className =
-        "absolute left-0 top-12 bottom-0 w-7 flex items-center justify-center z-10 bg-black bg-opacity-50 rounded-r-lg hover:bg-opacity-75 transition-all";
+        "absolute left-0 top-[46px] bottom-[-2px] w-[30px] flex items-center justify-center z-10 bg-black bg-opacity-50 rounded-r-lg hover:bg-opacity-75 transition-all";
       carousel_previous.innerHTML =
         "<img src='../assets/chevron_left_24dp.png' class='h-4 w-4' />";
 
@@ -200,7 +200,7 @@ function home_constructor() {
 
       const carousel_next = document.createElement("button");
       carousel_next.className =
-        "absolute right-0 top-12 bottom-0 w-7 flex items-center justify-center z-10 bg-black bg-opacity-50 rounded-l-lg hover:bg-opacity-75 transition-all";
+        "absolute right-0 top-[46px] bottom-[-2px] w-[30px] flex items-center justify-center z-10 bg-black bg-opacity-50 rounded-l-lg hover:bg-opacity-75 transition-all";
       carousel_next.innerHTML =
         "<img src='../assets/chevron_right_24dp.png' class='h-4 w-4' />";
 
@@ -222,38 +222,16 @@ function home_constructor() {
       categorie.items.forEach((item: any, i: number) => {
         const item_node = document.createElement("div");
         item_node.className =
-          "flex items-center justify-center flex-shrink-0 relative";
-        item_node.style.width = "calc((100vw - 4rem) / 7)";
-        item_node.style.height = "calc(((100vw - 4rem) / 7) * 1.3/1)";
+          "flex items-center justify-center flex-shrink-0 mx-[.25rem] overflow-hidden rounded-lg";
+        item_node.style.width = "calc(((100vw - 4rem) / 7) - .5rem)";
+        item_node.style.height = "calc(((100vw - 4rem) / 7) - .5rem) * 1.3/1)";
 
         categorie_carousel.appendChild(item_node);
 
-        const item_hover = document.createElement("div");
-        item_hover.className =
-          "absolute w-[calc(100%-.5rem)] h-full bg-neutral-800 rounded-lg overflow-hidden hover:z-10 hover:w-[110%] hover:h-[calc(110%+4rem)] transition-all ease-in-out duration-500";
-
-        if ((i + 1) % 7 == 1) {
-          item_hover.classList.add("left-[.25rem]");
-        } else if ((i + 1) % 7 == 0) {
-          item_hover.classList.add("right-[.25rem]");
-        }
-        item_node.appendChild(item_hover);
-
-        item_hover.addEventListener("click", () =>
-          watch_callback(item.redirect),
-        );
-
         const item_image = document.createElement("div");
-        item_image.className = "w-full aspect-[1/1.36]";
+        item_image.className = "w-full aspect-[1/1.3]";
 
-        item_hover.appendChild(item_image);
-
-        const item_title = document.createElement("h3");
-        item_title.className =
-          "h-8 font-medium text-sm p-2 text-nowrap w-full truncate";
-        item_title.textContent = item.title;
-
-        item_hover.appendChild(item_title);
+        item_node.appendChild(item_image);
 
         const asyncImage = new Image();
         asyncImage.src = `https://aniworld.to${item.image}`;
@@ -262,11 +240,126 @@ function home_constructor() {
         asyncImage.addEventListener("load", () => {
           item_image.appendChild(asyncImage);
         });
+
+        item_node.addEventListener("mouseenter", async () => {
+          const item_hover = document.createElement("div");
+          item_hover.className =
+            "absolute bg-neutral-900 rounded-lg overflow-hidden hover:z-20 transition-all ease-in-out duration-500 cursor-pointer";
+
+          const updateSize = () => {
+            const itemRect = item_node.getBoundingClientRect();
+            item_hover.style.height = `${itemRect.height}px`;
+            item_hover.style.width = `${itemRect.width}px`;
+            item_hover.style.top = `${itemRect.top}px`;
+            item_hover.style.left = `${itemRect.left}px`;
+          };
+          updateSize();
+
+          window.addEventListener("resize", updateSize);
+
+          let expanded = false;
+
+          item_hover.addEventListener("mouseleave", () => {
+            updateSize();
+            if (expanded) {
+              return setTimeout(() => item_hover.remove(), 500);
+            }
+            item_hover.remove();
+          });
+
+          document.body.appendChild(item_hover);
+
+          const hover_image = item_image.cloneNode(true);
+          item_hover.appendChild(hover_image);
+
+          item_hover.addEventListener("click", (e: Event) => {
+            watch_callback(item.redirect);
+          });
+
+          const item_title = document.createElement("h3");
+          item_title.className =
+            "h-8 font-medium text-sm p-2 text-nowrap w-full truncate";
+          item_title.textContent = item.title;
+
+          item_hover.appendChild(item_title);
+
+          if (localStorage.getItem("token")) {
+            const [getList, setList, subscribeList] = createState(0);
+
+            const on_list = document.createElement("div");
+            on_list.className =
+              "w-fit flex items-center space-x-1 bg-neutral-800 hover:bg-neutral-700 px-1 py-0.5 mx-2 rounded";
+            on_list.textContent = "...";
+            on_list.id = "on_list";
+
+            item_hover.appendChild(on_list);
+
+            subscribeList((newList) => {
+              console.log(newList);
+              if (newList == 1) {
+                on_list.innerHTML =
+                  "<img src='../assets/remove_24dp.png' class='h-2 w-2' /><span class='text-xs'>My List</span>";
+              } else {
+                on_list.innerHTML =
+                  "<img src='../assets/add_24dp.png' class='h-2 w-2' /><span class='text-xs'>My List</span>";
+              }
+            });
+
+            const mylist = JSON.parse(localStorage.getItem("mylist") || "")
+              .mylist.items;
+
+            const index = mylist.findIndex(
+              (a: any) => a.redirect == item.redirect,
+            );
+
+            console.log(index);
+
+            if (index !== -1) {
+              setList(1);
+            } else {
+              setList(0);
+            }
+
+            on_list.addEventListener("click", async (e) => {
+              e.stopPropagation();
+              const res = await fetch("http://animenetwork.org/handle-marked", {
+                method: "POST",
+                headers: {
+                  Authorization: localStorage.getItem("token") || "",
+                },
+                body: item.redirect.toString(),
+              });
+              if (!res.ok) return;
+              mylist_handler(getList() == 0 ? "add" : "rm", {
+                redirect: item.redirect,
+                image: item.image,
+                title: item.title,
+              });
+              setList(getList() == 0 ? 1 : 0);
+            });
+          }
+
+          setTimeout(() => {
+            expanded = true;
+            const itemRect = item_node.getBoundingClientRect();
+            item_hover.style.height = `${itemRect.height * 1.1 + 64}px`;
+            item_hover.style.width = `${itemRect.width * 1.1}px`;
+            if ((i + 1) % 7 == 1) {
+              item_hover.style.left = `${itemRect.left}px`;
+            } else if ((i + 1) % 7 == 0) {
+              item_hover.style.left = `${itemRect.left - itemRect.width * 0.1}px`;
+            } else {
+              item_hover.style.left = `${itemRect.left - itemRect.width * 0.05}px`;
+            }
+            item_hover.style.top = `${itemRect.top - itemRect.height * 0.05 - 32}px`;
+            item_hover.style.boxShadow = "0 4px 12px 0 rgba(0, 0, 0, 0.75)";
+          }, 500);
+        });
       });
     });
   };
 
-  const mylist_handler = async (method: String, data?: anime_data) => {
+  async function mylist_handler(method: String, data?: anime_data) {
     let storage = null;
     let current = null;
     if (localStorage.getItem("mylist")) {
@@ -294,7 +387,7 @@ function home_constructor() {
     }
 
     render();
-  };
+  }
 
   const setParams = (area: HTMLElement, callback: any) => {
     content = area;
