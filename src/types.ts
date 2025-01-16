29921e500f935ef6511371f945eaf358955a01e1
FEAR939 <rogronin8877@gmail.com>
2025-01-17 00:19:29 +0100

    moved all types in this file

---
+++ src/types.ts
@@ -0,0 +1,59 @@
+export type Subscriber<T> = (value: T) => void;
+export type SetStateFunction<T> = (value: T | ((prev: T) => T)) => void;
+export type GetStateFunction<T> = () => T;
+export type SubscribeFunction<T> = (callback: Subscriber<T>) => () => void;
+export type CreateStateReturn<T> = [
+  GetStateFunction<T>,
+  SetStateFunction<T>,
+  SubscribeFunction<T>,
+];
+
+export type categorie = {
+  label: string;
+  items: anime_data[];
+};
+
+export type search_result = {
+  title: string;
+  link: string;
+};
+
+export type anime_data = {
+  redirect: string;
+  image: string;
+  title: string;
+};
+
+export type anime = {
+  series_id: string;
+};
+
+export type anime_details = {
+  title: string;
+  desc: string;
+  image: string;
+  rating: string;
+  imdb: string;
+  seasons: season[];
+};
+
+export type season = {
+  label: string;
+  redirect: string;
+};
+
+export type api_episode = {
+  episode_id: string;
+  watch_duration: number;
+  watch_playtime: number;
+};
+
+export type episode = {
+  redirect: string;
+  title: string;
+  image: string;
+  duration: number;
+  playtime: number;
+  id: string;
+  watched: ((duration: number, playtime: number) => Promise<void>) | null;
+};
