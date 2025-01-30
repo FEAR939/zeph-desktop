e152b0e6784228cf1b19ece54c4438b6b6982328
FEAR939 <rogronin8877@gmail.com>
2025-01-30 21:05:45 +0100

    changed ui of the sign in and sign up pages

--- src/auth_module.ts
+++ src/auth_module.ts
@@ -1,5 +1,6 @@
 import { fetch } from "@tauri-apps/plugin-http";
 import createState from "./createstate";
+import { SubscribeFunction } from "./types";
 
 const [getRegister, setRegister, subscribeRegister] = createState(false);
 
@@ -40,7 +41,7 @@ async function register(username: string, password: string) {
   }
 }
 
-function auth_constructor() {
+function auth_constructor(loginSignal: SubscribeFunction<object | null>) {
   let content: HTMLElement | null = null;
   let home_callback: ((method: string) => void) | null = null;
   let login_callback: ((token: string) => void) | null = null;
@@ -57,34 +58,35 @@ function auth_constructor() {
 
     const username_field = document.createElement("input");
     username_field.className =
-      "w-96 px-4 py-2 rounded-full bg-neutral-900 rounded-lg placeholder:text-white placeholder:font-medium text-white font-medium";
-    username_field.placeholder = "username";
+      "w-96 px-4 py-2 rounded-full bg-neutral-900 rounded-lg placeholder:text-neutral-700 placeholder:font-medium text-white font-medium";
+    username_field.placeholder = "Username";
 
     login_node.appendChild(username_field);
 
     const password_field = document.createElement("input");
     password_field.className =
-      "w-96 px-4 py-2 rounded-full bg-neutral-900 rounded-lg placeholder:text-white placeholder:font-medium text-white font-medium";
-    password_field.placeholder = "password";
+      "w-96 px-4 py-2 rounded-full bg-neutral-900 rounded-lg placeholder:text-neutral-700 placeholder:font-medium text-white font-medium";
+    password_field.placeholder = "Password";
     password_field.type = "password";
 
     login_node.appendChild(password_field);
 
     const submit_node = document.createElement("button");
     submit_node.className =
-      "w-96 px-4 py-2 rounded-full bg-neutral-900 rounded-lg text-white font-medium cursor-pointer";
+      "w-96 px-4 py-2 mt-4 rounded-full bg-neutral-900 rounded-lg text-white font-medium cursor-pointer";
 
     subscribeRegister((newRegister) => {
       if (newRegister) {
-        submit_node.textContent = "Register";
+        submit_node.textContent = "Sign up";
       } else {
-        submit_node.textContent = "Login";
+        submit_node.textContent = "Sign in";
       }
     });
 
     login_node.appendChild(submit_node);
 
     function statusgood() {
+      submit_node.classList.remove("animate-pulse");
       submit_node.classList.toggle("bg-neutral-900");
       submit_node.classList.add("bg-green-600");
       submit_node.textContent = "Success!";
@@ -94,14 +96,13 @@ function auth_constructor() {
         submit_node.classList.add("bg-blue-600");
 
         if (getRegister()) {
-          submit_node.textContent = "Go to Login";
-        } else {
-          submit_node.textContent = "Go to Home";
+          submit_node.textContent = "Continue Sign in";
         }
       }, 3000);
     }
 
     function statusbad() {
+      submit_node.classList.remove("animate-pulse");
       submit_node.classList.toggle("bg-neutral-900");
       submit_node.classList.add("bg-red-600");
       submit_node.textContent = "Something went wrong...";
@@ -109,11 +110,12 @@ function auth_constructor() {
       setTimeout(() => {
         submit_node.classList.toggle("bg-neutral-900");
         submit_node.classList.remove("bg-red-600");
-        submit_node.textContent = "Login";
+        submit_node.textContent = "Sign in";
       }, 3000);
     }
 
     submit_node.addEventListener("click", async () => {
+      submit_node.classList.add("animate-pulse");
       if (!localStorage.getItem("token")) {
         if (getRegister()) {
           const resStatus = await register(
@@ -139,8 +141,6 @@ function auth_constructor() {
             return;
           }
 
-          statusgood();
-
           if (login_callback == null) return;
           login_callback(token);
         }
@@ -151,21 +151,60 @@ function auth_constructor() {
     });
 
     const changeRegister = document.createElement("div");
-    changeRegister.className = "h-8 text-sm";
+    changeRegister.className = "h-8 w-96 text-sm text-neutral-600";
 
     login_node.appendChild(changeRegister);
 
     subscribeRegister((newRegister) => {
       if (newRegister) {
-        changeRegister.textContent = "Login with existing account instead.";
+        changeRegister.innerHTML =
+          "Already have an account? <span class='text-white cursor-pointer'>Sign in.</span>";
       } else {
-        changeRegister.textContent = "Create a new account.";
+        changeRegister.innerHTML =
+          "Don't have an account? <span class='text-white cursor-pointer'>Sign up.</span>";
       }
     });
 
     changeRegister.addEventListener("click", () => setRegister(!getRegister()));
 
     setRegister(false);
+
+    loginSignal((newLogin) => {
+      if (newLogin == null) return;
+      login_node.innerHTML = "";
+
+      const userModal = document.createElement("div");
+      userModal.className =
+        "h-fit w-96 p-4 flex flex-col space-y-1 overflow-hidden border border-white/5 rounded-lg";
+
+      const modalMessage = document.createElement("div");
+      modalMessage.className = "w-full";
+      modalMessage.textContent = "You're logged in as";
+
+      userModal.appendChild(modalMessage);
+
+      const user = document.createElement("div");
+      user.className = "w-full mb-8 flex space-x-2 items-center";
+      user.innerHTML = `
+        <img src="${newLogin.avatar_url}" class="h-8 w-8 rounded-full" />
+        <span class="font-medium">${newLogin.username}</span>
+        `;
+
+      userModal.appendChild(user);
+
+      const takeBack = document.createElement("button");
+      takeBack.className = "w-full p-2 bg-blue-800 rounded-full cursor-pointer";
+      takeBack.textContent = "Continue";
+
+      userModal.appendChild(takeBack);
+
+      takeBack.addEventListener("click", () => {
+        if (home_callback == null) return;
+        home_callback("update");
+      });
+
+      login_node.appendChild(userModal);
+    });
   };
 
   const setParams = (
