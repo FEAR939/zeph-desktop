122ec34c914813db4737c47dc84c8800fe695d9e
F E A R <rogronin8877@gmail.com>
2025-06-19 04:35:55 +0200

    first touches to the UI with the new UI design language

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,7 @@ import { AccountSettings } from "./components/settings_account";
 
 const settings = [
   {
+    icon: "./icons/person_24dp.png",
     label: "Account",
     fn: AccountSettings,
   },
@@ -11,13 +12,13 @@ export function Settings(userState, auth_page) {
   const build = () => {
     const settingsWrapper = document.createElement("div");
     settingsWrapper.className =
-      "absolute z-40 inset-0 h-full w-full flex justify-center bg-black/25";
+      "absolute z-40 inset-0 h-full w-full flex justify-center bg-neutral-950/60 backdrop-blur-xl";
 
     const close = () => settingsWrapper.remove();
 
     const settingsPanel = document.createElement("div");
     settingsPanel.className =
-      "min-h-[calc(100%-1rem)] h-full w-[64rem] max-w-full bg-neutral-900 mt-4 overflow-hidden rounded-t-lg border-box";
+      "min-h-[calc(100%-1rem)] h-full w-[64rem] max-w-full mt-4 overflow-hidden border-box";
 
     const closePanel = document.createElement("img");
     closePanel.className = "m-4 h-8 w-8 cursor-pointer";
@@ -44,8 +45,9 @@ export function Settings(userState, auth_page) {
 
     settings.map((setting) => {
       const navSetting = document.createElement("div");
-      navSetting.className = "px-2 py-1 cursor-pointer text-sm";
-      navSetting.textContent = setting.label;
+      navSetting.className =
+        "flex space-x-2 items-center px-2 py-1 cursor-pointer text-sm hover:bg-neutral-950/60 hover:backdrop-blur-xl hover:outline hover:outline-neutral-800 rounded-lg";
+      navSetting.innerHTML = `<img src="${setting.icon}" alt="${setting.label}" class="h-4 w-4"/><div>${setting.label}</div>`;
 
       main_nav.appendChild(navSetting);
 
